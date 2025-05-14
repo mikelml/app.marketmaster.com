59d459c789954267924ef80b10d859a7c112103b
Mike lml <miguelshunidgo@hotmail.com>
2025-05-14 15:35:45 -0600

    cahnges

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   server: {
     port: process.env.PORT as unknown as number,
